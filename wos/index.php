88d9adbad38eade3d78ae099d9ffa57a6863c309
m13kj <jokey13@bk.ru>
2018-03-12 16:30:05 +0300

    Разработка началась

---
+++ wos/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="assets/style.css">
+	<title>World of session</title>
+</head>
+<body>
+	<header>
+		<?php 
+			include_once "menu.php";
+		 ?>
+	</header>
+</body>
+</html>
