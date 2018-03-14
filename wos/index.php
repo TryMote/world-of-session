9baa57d6ac04047eda346287c970922b6d0a2879
m13kj <jokey13@bk.ru>
2018-03-14 06:08:52 +0300

    Забыл обновить

--- wos/index.php
+++ wos/index.php
@@ -7,9 +7,35 @@
 </head>
 <body>
 	<header>
-		<?php 
-			include_once "menu.php";
-		 ?>
+		<div class="main">
+			
+			<div id="logo">
+				<a href="index.php">World of session</a>
+			</div>
+
+			<div id="main-menu">
+				<a href="lections.php">Лекции</a>
+				<a href="lections.php">Практика</a>
+				<a href="lections.php">Контакты</a>
+				<span>Вход</span>
+			</div>
+
+			<div id="vxod">
+				<form action="signin.php" method="post">
+					<input type="text" name="login" placeholder="Введите логин"><br/>
+					<input type="pas" name="password" placeholder="Введите пароль"><br/>
+					<a href="register.php">Регистрация</a><br>
+					<a href="forgotpass.php">Забыли пароль?</a><br>
+					<input type="submit" value="Войти">
+				</form>
+			</div>
+
+		</div>
 	</header>
+	<footer>
+		<div id="copy">
+			Copyright 2018 &copy;
+		</div>
+	</footer>
 </body>
 </html>
