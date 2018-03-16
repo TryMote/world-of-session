bdda8cd75c3881be4d9e3edc80f45a83e7cb1a7d
m13kj <trymote@mail.ru>
2018-03-16 12:42:17 +0300

    scripts

---
+++ wos/scripts/main.php
@@ -0,0 +1,25 @@
+<div class="main">
+			
+			<div id="logo">
+				<a href="index.html">World of session</a>
+			</div>
+
+			<div id="main-menu">
+				<a href="lections.html">Лекции</a>
+				<a href="practic.html">Практика</a>
+				<a href="contact.html">Контакты</a>
+				<span>Вход</span>
+			</div>
+
+			<div id="vxod">
+				<form action="signin.html" method="post">
+					<input type="text" name="login" placeholder="Введите логин"><br/>
+					<input type="pas" name="password" placeholder="Введите пароль"><br/>
+					<a href="register.html">Регистрация</a><br>
+					<a href="forgotpass.html">Забыли пароль?</a><br>
+					<input type="submit" value="Войти">
+				</form>
+			</div>
+
+		</div>
+
