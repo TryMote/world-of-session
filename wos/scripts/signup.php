54dbb4f85ca74a0139ae093c8698ff8367ea5c32
m13kj <trymote@mail.ru>
2018-03-17 18:25:03 +0300

    17.03.18 18

--- wos/scripts/signup.php
+++ wos/scripts/signup.php
@@ -9,22 +9,32 @@
 	$query = "LOCK TABLES $upd WRITE";
 	$result = $conn->query($query);
 	if(!$result) error_page('srl_upd');
-	$query = "INSERT INTO $upd VALUES(NULL, '$first_name', '$last_name', '$email')";
-	$result = $conn->query($query);
+	$query = "INSERT INTO $upd(first_name, last_name, email) VALUES(?,?,?)";
+	$result = $conn->prepare($query);
 	if(!$result) error_page('sri_upd');
+	$result->bind_param('sss', $first_name, $last_name, $email);
+	$result->execute();
+	if(!$result->affected_rows) error_page('srn_rupd');
 	$insertID = $conn->insert_id;
 	$query = "UNLOCK TABLES";
 	$result = $conn->query($query);
 	if(!$result) error_page('srul_upd');
 	
-	$query = "INSERT INTO $usd(user_id, image) VALUES($insertID, '$image_name')";
-	$result = $conn->query($query);
+	$query = "INSERT INTO $usd(user_id, image) VALUES(?,?)";
+	$result = $conn->prepare($query);
 	if(!$result) error_page('sri_usd');  	
+	$result->bind_param('is',$insertID, $image_name);
+	$result->execute();
+	if(!$result->affected_rows) error_page('srn_rusd');
 
-	$query = "INSERT INTO $s_i(user_id, email, nickname, password) VALUES($insertID,'$email', '$login', '$pass')";
-	$result = $conn->query($query);
+	$query = "INSERT INTO $s_i(user_id, email, nickname, password) VALUES(?,?,?,?)";
+	$result = $conn->prepare($query);
 	if(!$result) error_page('sri_si');
+	$result->bind_param('isss', $insertID, $email, $login, $pass);
+	$result->execute();
+	if(!$result->affected_rows) error_page('srn_rsi');
 	
+	$result->close();	
 	$conn->close();
 	mail($email, "Верификация вашей почты", "Привет");
 	echo "
